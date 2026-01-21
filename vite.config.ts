a89d78a417cd70d590e6e193218a7639d14acbb9
rashidrk201111 <141550893+rashidrk201111@users.noreply.github.com>
2026-01-21 08:25:31 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: './',  // <--- important for deployed apps
+  base: './',
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(
+      process.env.VITE_SUPABASE_URL || 'https://ftjukgofugzoxhvqhrez.supabase.co'
+    ),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(
+      process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0anVrZ29mdWd6b3hodnFocmV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NjQyMDksImV4cCI6MjA3NzI0MDIwOX0.j8kwtLaklvEB59X4_ir3HBGGkUmXfFbnl4M8lFC8wZM'
+    ),
+  },
 });
